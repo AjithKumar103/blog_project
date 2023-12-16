2de8e386052df4dc9bba14de7126d54e9d06feaa
Ajith <appuajith1010@gmail.com>
2023-12-16 19:57:33 +0530

    Second Commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-b9oe@@@&p7bhmq&s%31fharh3p(p$sm@b29qxlj!rxrdeuk195
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
